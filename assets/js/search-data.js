f1509813d8059d39d178e0cd89c4e91eceedac15
dulbrich24 <d.ulb@web.de>
2026-02-28 16:13:39 +0000

    Deploying to gh-pages from @ dulbrich24/dulbrich24.github.io@7b0891d1fbd27253b2c477f6a3f7143d3c33a958 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-currently-i-am-looking-for-a-full-time-position-in-mathematical-research-or-teaching-pure-or-applied-at-a-university",
+          title: 'Currently, I am looking for a full-time position in mathematical research or teaching...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
