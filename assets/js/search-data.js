ebdaab831cee1407651f74de5c30871d5209c1ec
dulbrich24 <d.ulb@web.de>
2026-02-28 18:59:09 +0000

    Deploying to gh-pages from @ dulbrich24/dulbrich24.github.io@d237fcb7481c0e60ce2d0a6749c9b504173a60e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -517,7 +517,7 @@ ninja.data = [{
         title: 'arXiv',
         section: 'Socials',
         handler: () => {
-          window.open("https://arxiv.org/a/DUlbrich.html", "_blank");
+          window.open("https://arxiv.org/a/ulbrich_d_1.html", "_blank");
         },
       },{
         id: 'social-orcid',
