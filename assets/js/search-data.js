760ea2599314ba6674bde01c1cce762e31b70235
dulbrich24 <d.ulb@web.de>
2026-02-28 16:36:22 +0000

    Deploying to gh-pages from @ dulbrich24/dulbrich24.github.io@c18c8589f8b2837af603d4260d54c72ce9376e4f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -503,21 +503,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_UlbrichDennis.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%75%6C%62%72%69%63%68.%64%65%6E%6E%69%73@%69%63%6C%6F%75%64.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -531,14 +524,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=b1u5plUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
