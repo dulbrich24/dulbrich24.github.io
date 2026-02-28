18f0768b74f9a37e3dd4210acb1781a9cb331e47
dulbrich24 <d.ulb@web.de>
2026-02-28 18:26:00 +0000

    Deploying to gh-pages from @ dulbrich24/dulbrich24.github.io@a73621c84ad46ab2b163874ebc533bea59b31b96 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -517,7 +517,7 @@ ninja.data = [{
         title: 'arXiv',
         section: 'Socials',
         handler: () => {
-          window.open("https://arxiv.org/a/search/math?searchtype=author&query=Ulbrich%2C+D.html", "_blank");
+          window.open("https://arxiv.org/a/author&query=Ulbrich%2C+D.html", "_blank");
         },
       },{
         id: 'social-orcid',
